b9d27e6c7c07da1150a36debcbee0e36b214a5aa
Khrisvana <krisnaputu2@gmail.com>
2024-03-06 15:53:14 +0800

    adding comment

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="main-wrapper">
         <div class="content row justify-content-center register">
+            <!-- SECTION LOGIN -->
             <SectionLogin/>
+            <!-- !SECTION LOGIN -->
         </div>
     </div>
 </template>
